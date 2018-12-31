b96987f61a6376c52b8727fbcc4048f9d10aa252
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-12-31 15:52:56 -0200

    FEA: Criação de controller de feed e resources
    Fix #6

---
+++ app/Http/Controllers/Api/V1/FeedController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Exceptions\Api\V1\ApiException;
+use App\Http\Resources\V1\FeedNew;
+use App\Http\Resources\V1\FeedNewCollection;
+use App\Models\V1\NewFeed as NewFeedModel;
+use App\Models\V1\NewFeed;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * Constroller com recursos do feed de notícias.
+ * .
+ * @package App\Http\Controllers\Api\V1
+ */
+class FeedController extends Controller
+{
+    /**
+     * Método responsável por retornar uma lista de publicações
+     * através da API.
+     */
+    public function listNews(Request $request)
+    {
+        $perPage = $request->get('per_page', 15);
+
+        $page = $request->get('page', 1);
+
+        $news = NewFeedModel::orderBy('publication_date', 'DESC')
+            ->paginate($perPage, ['*'], 'page', $page);
+
+        return new FeedNewCollection($news);
+    }
+
+    /**
+     * Método repsonsável por retornar um único recurso de uma publicação
+     * através da API.
+     *
+     * @param string $identifier Identificador da publicação.
+     * @return FeedNew
+     * @throws ApiException
+     */
+    public function showNews($identifier)
+    {
+        $newsFeed = NewFeedModel::find($identifier);
+
+        if (empty($newsFeed)) {
+            throw new ApiException('Recurso de publicação não encontrado.');
+        }
+
+        return new FeedNew($newsFeed);
+    }
+}
